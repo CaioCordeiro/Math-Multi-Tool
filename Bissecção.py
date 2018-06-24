30f599365bc43ca93b8b2a605b5baeeb3bb64d2e
CaioCordeiro <37188299+CaioCordeiro@users.noreply.github.com>
2018-06-24 15:23:02 -0300

    Update Bissecção.py

--- Bissecção.py
+++ Bissecção.py
@@ -1,19 +1,31 @@
-#Metodo da Bissecção
+from math import *
+import math
+def trun_n_d(n,d):
+    s=repr(n).split('.')
+    if (len(s)==1):
+        return int(s[0])
+    return float(s[0]+'.'+s[1][:int(d)])
 def f(x):
-   f = x**2 + 4*x + 8
+   f = sin(x) - 1/x
    return f
+
 a = float(input("Insira a: "))
 b = float(input("Insira b: "))
 p = float(input("Insira a precisão: "))
-
+pl = 10**(-p)
 if (f(a)*f(b) < 0):
-   x= (a+b)/2
-   while (f(x)>p):
-      if (f(a)*f(b)<0):
+   x = (a+b)/2
+   if (f(x)*f(b)<0):
+      a = x
+   else:
+      b = x
+   while (abs(f(x))>pl):
+      x = (a+b)/2
+      if (f(x)*f(b)<0):
          a = x
       else:
          b = x
-      x = (a+b)/2
-   print("A raiz da função , com precisão de {} , é {}".format(p,x))
+   x = trun_n_d(x,p)
+   print("A raiz da função , com precisão de {} casas decimais , é {}".format(p,x))
 else:
-   print("Não tem raiz no intervalo")
+   print("Não tem raiz unica no intervalo")
