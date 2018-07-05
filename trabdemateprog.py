4fb0420750a844c49fd8b8b4dd0f4adf2f40b6e0
CaioCordeiro <37188299+CaioCordeiro@users.noreply.github.com>
2018-07-04 23:29:04 -0300

    até agora tem o metodo de jacobi e de bissecção

---
+++ trabdemateprog.py
@@ -0,0 +1,245 @@
+'''
+
+
+		LIBS
+
+
+'''
+from math import *
+import math
+from pprint import pprint
+from numpy import array, zeros, diag, diagflat, dot
+
+'''
+
+
+		METODO DE JACOBI
+
+
+'''
+
+def jacobi(A, b, N=2000):
+
+    x = zeros(len(A[0]))
+    x_ant = zeros(len(A[0]))
+    D = diag(A)
+    R = A - diagflat(D)
+
+    for i in range(N):
+        x = (b - dot(R, x)) / D
+        if(x.all() == x_ant.all()):
+            return x
+        x_ant = x
+    return x
+
+
+def _print_arrays(number_of_variables):
+    print("\n\n\n Your table:\n\n")
+    for collumn in range(0, number_of_variables):
+        print("|-----", end='')
+    print("|   |----|   |----|")
+
+    for row in range(0, number_of_variables):
+        for collumn in range(0, number_of_variables):
+            print("| A"+str(row)+str(collumn), end=' ')
+        if(row == number_of_variables//2):
+            print("| . | X"+str(row)+" | = | b"+str(row)+" |")
+        else:
+            print("|   | X"+str(row)+" |   | b"+str(row)+" |")
+
+    for collumn in range(0, number_of_variables):
+        print("|-----", end='')
+    print("|   |----|   |----|\n\n\n")
+
+
+def _print_arrays_final(number_of_variables, A, b, x):
+    print("\n\n\n Your table:\n\n")
+    for collumn in range(0, number_of_variables):
+        print("|------", end='')
+    print("|   |-------|   |------|")
+
+    for row in range(0, number_of_variables):
+        for collumn in range(0, number_of_variables):
+            print("| "+"{0:.2f}".format(A[row][collumn]), end=' ')
+        if(row == number_of_variables//2):
+            print("| . | "+"{0:.2f}".format(x[row]) +
+                  " | = | "+"{0:.2f}".format(b[row])+" |")
+        else:
+            print("|   | "+"{0:.2f}".format(x[row]) +
+                  " |   | "+"{0:.2f}".format(b[row])+" |")
+
+    for collumn in range(0, number_of_variables):
+        print("|------", end='')
+    print("|   |------|   |------|\n\n\n")
+
+
+def _get_A_array(number_of_variables):
+    temp_collumn = []
+    A = []
+    for row in range(0, number_of_variables):
+        for collumn in range(0, number_of_variables):
+            temp_collumn.append(
+                float(input("Type the A"+str(row)+str(collumn)+" :")))
+        print("")
+        A.append(temp_collumn)
+        temp_collumn = []
+    return array(A)
+
+
+def _get_b_array(number_of_variables):
+    b = []
+    for variable in range(0, number_of_variables):
+        b.append(
+            float(input("Type the b" + str(variable) + " :")))
+    print("")
+    return array(b)
+
+# Start of the program
+
+def jacobi1():
+    number_of_variables = int(input("How many variables do you want to have? "))
+    _print_arrays(number_of_variables)
+    A = _get_A_array(number_of_variables)
+
+    b = _get_b_array(number_of_variables)
+
+    x = jacobi(A, b)
+
+    _print_arrays_final(number_of_variables, A, b, x)
+
+
+'''
+
+
+		METODO DA BISSECÇÃO
+
+
+'''
+
+def trun_n_d(n,d):
+    s=repr(n).split('.')#dando split no numero pelo "." criando uma lista com 2 valores , um com o numero inteiro e outro com um numero decimal
+    if (len(s)==1):
+        return int(s[0])#se s só tiver 1 valor é porque o numero ja é inteiro , por isso retorna ele mesmo
+    return float(s[0]+'.'+s[1][:int(d)])#retorna o numero inteira + "." + o 1 valor + uma sequencia de numeros definida por "d"
+
+def f(fx,x):#definindo a função matematica
+   f = eval(fx)
+   return f
+def bisseccao():
+   fx = input("Insira a função: ")
+
+   a = float(input("Insira a: "))#1º Valor do intervalo [a,b]
+   b = float(input("Insira b: "))
+   p = float(input("Insira a precisão(Entre 1 - 16): "))#Numero de casas decimais
+   pl = 10**(-p)#Definindo a precisão em numeros decimais 
+   if (f(fx ,a)*f(fx ,b) < 0):#Aplicando Bozzano
+      x = (a+b)/2#1ª media do intervalo
+      
+      if (f(fx,x)*f(fx,b)<0):#Define se o Numero achado será o novo "a" ou o novo "b"
+         a = x
+      else:
+         b = x
+      while (abs(f(fx,x))>pl):#Cria um loop em que ele só termina caso o numero atender a precisão
+         x = (a+b)/2
+         if (f(fx,x)*f(fx,b)<0):
+            a = x
+         else:
+            b = x
+      x = trun_n_d(x,p)#Trunca o  numero pra caber na precisão
+      print("A raiz da função , com precisão de {} casas decimais , é {}".format(p,x))
+   else:
+      print("Não tem raiz unica no intervalo")#Caso Bozzano esteja falso
+'''
+
+
+		DEFININDO A GRAPHIC INTERFACE
+
+
+'''
+gui1 = """
+======================================
+|Digite (1) para -> Zeros da Função  
+|Digite (2) para -> Interpolação	 
+|Digite (3) para -> Integração		 
+|Digite (4) para -> Sistemas Lineares
+|Digite (0) para -> SAIR			 
+======================================
+"""
+gui2 = """
+===============================
+|Digite (1) para -> Bissecção 
+|Digite (2) para -> Newton	  
+===============================
+"""
+gui3 = """
+===============================
+|Digite (1) para -> Newton    
+|Digite (2) para -> Lagrange  
+===============================
+"""
+gui4 = """
+===============================
+|Digite (1) para -> Trapézios 
+|Digite (2) para -> Simpsom	  
+===============================
+"""
+gui5 = """
+===============================
+|Digite (1) para -> Jacobi    
+|Digite (2) para -> LU   	  
+===============================
+"""
+guin = """
+===============================
+|Opção inválida no momento    |
+===============================
+"""
+guiout = """
+===============================
+|Obrigado e adeus!            |
+===============================
+"""
+'''
+
+
+		RODANDO O FLUXO DE TELAS
+
+
+'''
+while True:
+	x = int(input(gui1))
+
+	if x == 1:
+		y = int(input(gui2))
+		if y == 1:
+			bisseccao()
+			continue
+		else:
+			print(guin)
+			continue
+	elif x == 2:
+		y = int(input(gui3))
+		if y == 1:
+			#INSIRA O METODO DE NEWTON AQUI
+			continue
+		else:
+			print(guin)
+	elif x == 3:
+		y = int(input(gui4))
+		if y == 1:
+			#INSIRA O METODO DOS TRAPÉZIOS AQUI
+			continue
+		else:
+			print(guin)
+			continue
+	elif x == 4:
+		y = int(input(gui5))
+		if y == 1:
+			jacobi1()
+			continue
+		else:
+			print(guin)
+			continue
+	elif x == 0:
+		print(guiout)
+		break
