e41ab4f1c60473174124a781cde0725e91b922f8
CaioCordeiro <37188299+CaioCordeiro@users.noreply.github.com>
2018-07-05 00:34:36 -0300

    Eu mudei para  o codigo que apliquei no final
    o codigo de polux tava incompleto

--- simpson.py
+++ simpson.py
@@ -1,44 +1,27 @@
-# Método de Integração de Simpson
-# Função de integração: x * f(x) 
-def simpson():
-    while True:
 
-        x = []
-        fx = []
-        i = 0
-        I = 0
-        P = 0
-        n = int(input('Número de pontos: '))
+def f(fx,x):#definindo a função matematica
+   f = eval(fx)
+   return f
 
-        if (n % 2) != 0:
+def J():
+    f = input("Insira a função : ")
+    N =  float(input("Numero de pontos: "))
+    # lower & upper limits of the integral
+    a = float(input("Insira a : "))
+    b = float(input("Insira b : "))
 
-            a = float(input('Valor inicial: ')) 
-            b = float(input('Valor final: '))
-            h = (b - a)/n
+    # step size
+    h = (b - a) / N
 
-            while i < n:
-                num_x = float(input(' x{0}: '.format(i)))
-                num_fx = float(input(' fx{0}: '.format(i)))
-                x.append(num_x)
-                fx.append(num_fx)
-
-                if (i != 0) and ((i % 2) == 0) and (i != (n - 1)):
-                    P += x[i] * fx[i]
-                elif (i != 0) and ((i % 2) != 0) and (i != (n - 1)):
-                    I += x[i] * fx[i]
-
-                i += 1
-
-            E = (x[0] * fx[0]) + (x[n-1] * fx[n-1])
-
-            Simpson = (h / 3) * (E + (4 * I) + (2 * P))  
-
-            print('O valor da integral é: {0:.3f}'.format(Simpson))
-            
-            break
-            
+    # Sum the values for Simpson's integration
+    s = f(f, a) + f(f, b)
+    for i in range(1, N):
+        t = a + i * h
+        if i % 2 == 1:
+            s += 4.0 * f(f, t)
         else:
-            
-            print('O número de pontos precisa ser ímpar.')  
+            s += 2.0 * f(f, t)
 
-simpson()
+    # multiply by h/3 to get the integral
+    # and divide by pi to get the Bessel function.
+    print(s * h / (3.0)) 
