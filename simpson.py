eea86c673f7ea81a53455224f3969cbc3dc5e9a1
CaioCordeiro <37188299+CaioCordeiro@users.noreply.github.com>
2018-07-05 00:53:40 -0300

    update de bugs

--- simpson.py
+++ simpson.py
@@ -1,11 +1,9 @@
-
 def f(fx,x):#definindo a função matematica
    f = eval(fx)
    return f
-
-def J():
-    f = input("Insira a função : ")
-    N =  float(input("Numero de pontos: "))
+def simpson():
+    fx = input("Insira a função : ")
+    N =  int(input("Numero de pontos: "))
     # lower & upper limits of the integral
     a = float(input("Insira a : "))
     b = float(input("Insira b : "))
@@ -14,13 +12,13 @@ def J():
     h = (b - a) / N
 
     # Sum the values for Simpson's integration
-    s = f(f, a) + f(f, b)
+    s = f(fx, a) + f(fx, b)
     for i in range(1, N):
         t = a + i * h
         if i % 2 == 1:
-            s += 4.0 * f(f, t)
+            s += 4.0 * f(fx, t)
         else:
-            s += 2.0 * f(f, t)
+            s += 2.0 * f(fx, t)
 
     # multiply by h/3 to get the integral
     # and divide by pi to get the Bessel function.
